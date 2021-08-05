7559e534756262bbd9980db20774f9b5a54ff9c7
Mathis Barré <mathis.barre@live.fr>
2021-08-05 12:10:42 +0200

    feat: cors authorization

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const AutoLoad = require("fastify-autoload")
 
 module.exports = async function (fastify, opts) {
   // Place here your custom code!
+  fastify.register(require("fastify-cors"), {
+    origin: true
+  })
 
   // Do not touch the following lines
 
