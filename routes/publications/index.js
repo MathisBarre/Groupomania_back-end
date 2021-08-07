eb288522e3a55570d82cca771267e4d01a80405f
Mathis Barré <mathis.barre@live.fr>
2021-08-07 12:40:37 +0200

    feat: add get publications endpoints

--- routes/publications/index.js
+++ routes/publications/index.js
@@ -1,6 +1,15 @@
 "use strict"
 
 module.exports = async function (fastify, opts) {
+  fastify.get("/", async function(request, reply) {
+    const allPublication = await fastify.prisma.publication.findMany({
+      include: {
+        user: true
+      }
+    })
+    return allPublication
+  })
+
   fastify.post("/", async function(request, reply) {
     const newUser = await fastify.prisma.publication.create({
       data: {
