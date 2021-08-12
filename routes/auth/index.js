17a96e5516f2d0547cf438a925438c62e3506636
Mathis Barré <mathis.barre@live.fr>
2021-08-12 15:35:13 +0200

    feat: better login

--- routes/auth/index.js
+++ routes/auth/index.js
@@ -1,21 +1,57 @@
 "use strict"
 
 module.exports = async function (fastify, opts) {
-  fastify.get("/login", async function(request, reply) {
-    const token = await reply.jwtSign({
-      name: 'foo',
-      role: ['admin', 'spy']
-    })
-  
-    reply
-      .setCookie('token', token, {
-        domain: 'localhost',
-        path: '/',
-        secure: true, // send cookie over HTTPS only
-        httpOnly: true,
-        sameSite: true // alternative CSRF protection
+  fastify.post("/login", async function(request, reply) {
+      const { email, password } = request.body
+
+      const user = await fastify.prisma.user.findUnique({
+        where: {
+          email: email,
+        }
       })
-      .code(200)
-      .send('Cookie sent')
+
+      if (!user) throw fastify.httpErrors.unauthorized("User not found")
+
+      const token = await reply.jwtSign({ role: user.role })
+    
+      if (user.password !== password) throw fastify.httpErrors.unauthorized("Incorrect password")
+
+      reply
+        .setCookie("token", token, {
+          domain: "localhost",
+          path: "/",
+          secure: true, // send cookie over HTTPS only
+          httpOnly: true,
+          sameSite: true // alternative CSRF protection
+        })
+        .setCookie("connectedUser", JSON.stringify({
+          displayName: user.display_name,
+          email: user.email,
+          role: user.role,
+          profileImageUrl: user.profile_image_url
+        }), {
+          domain: "localhost",
+          path: "/",
+          secure: true, // send cookie over HTTPS only
+          httpOnly: false,
+          sameSite: true // alternative CSRF protection
+        })
+        .code(200)
+        .send({
+          displayName: user.display_name,
+          email: user.email,
+          role: user.role,
+          profileImageUrl: user.profile_image_url
+        })
+  })
+
+  fastify.post("/logout", async function(request, reply) {
+    reply.clearCookie('token', {
+      domain: "localhost",
+      path: "/",
+      secure: true, // send cookie over HTTPS only
+      httpOnly: true,
+      sameSite: true // alternative CSRF protection
+    }).code(200).send({ message: "User disconnected"})
   })
 }
