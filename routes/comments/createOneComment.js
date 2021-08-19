1e0ea4172ca3af3599502e0e70b9f4657e31f6fd
Mathis Barré <mathis.barre@live.fr>
2021-08-19 14:26:22 +0200

    feat: add schema to comment creation endpoint

--- routes/comments/createOneComment.js
+++ routes/comments/createOneComment.js
@@ -2,19 +2,42 @@ export default async function(fastify) {
   fastify.route({
     method: "POST",
     url: "/comments",
+    schema: schema,
     preValidation: [fastify.authenticate],
     handler: handler
   })
 
   async function handler(request, reply) {
-    const newComment = await fastify.prisma.comment.create({
+    const { comment, publicationId } = request.body
+    const { userId } = fastify.jwt.decode(request.cookies.token)
+
+    await fastify.prisma.comment.create({
       data: {
-        content: request.body.comment,
-        publication_id: request.body.publicationId,
-        author_id: fastify.jwt.decode(request.cookies.token).userId
+        content: comment,
+        publication_id: publicationId,
+        author_id: userId
       }
     })
 
-    return newComment
+    return { message: "Comment added successfully !"}
+  }
+}
+
+const schema = {
+  body: {
+    type: "object",
+    properties: {
+      comment: { type : "string" },
+      publicationId: { type: "number" }
+    }
+  },
+
+  response: {
+    200: {
+      type: "object",
+      properties: {
+        message: { type: "string" }
+      }
+    }
   }
 }
